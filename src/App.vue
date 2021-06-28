e99c56d9bd50863fc89c1db18853b954f0496800
bcandas <candasbertrand@gmail.com>
2021-06-28 12:59:30 +0200

    update quizz

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+ <Question @answer="onAnswer" label="question ?" :answers="[{label: 'answer 1'},{label: 'answer 2'},{label: 'answer 3'} ]"/>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+<script>
+import Question from "./components/Question.vue";
+export default {
+  components: {Question},
+  mounted() {
+  },
+  methods : {
+    onAnswer(index) {
+      console.log('index', index);
+    }
+  }
+}
 </script>
 
 <style>
