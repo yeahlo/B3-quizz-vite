78d30b406f8e970e69d2729946e6f23109957bbc
bcandas <candasbertrand@gmail.com>
2021-06-28 12:59:43 +0200

    update quizz

---
+++ src/components/Button.vue
@@ -0,0 +1,3 @@
+<template>
+  <button class="border-2 border-none bg-blue-400 mx-2 p-1.5"><slot /></button>
+</template>
---
+++ src/components/Question.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-col items-center">
+    <span class="mb-2">{{label}}</span>
+    <div class="flex">
+      <Button @click="onClick(index)" v-for="(answer, index) in answers">{{answer.label}}</Button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  components: {Button},
+  props : ['answers', 'label'],
+  data : () => ({
+
+  }),
+  mounted() {
+  },
+  methods : {
+    onClick(index) {
+      this.$emit('answer',index);
+    }
+  }
+}
+</script>
